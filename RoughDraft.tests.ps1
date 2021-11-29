482aa8b30ccbf87cc8de63db9743cb5ef1e4d754
James Brundage <james.brundage@start-automating.com>
2021-11-29 01:48:02 -0800

    Updating tests (improving coverage)

--- RoughDraft.tests.ps1
+++ RoughDraft.tests.ps1
@@ -1,8 +1,10 @@
 ﻿if ($PSVersionTable.Platform -eq 'Unix') {
     $ffMpegInPath =  $ExecutionContext.SessionState.InvokeCommand.GetCommand('ffmpeg', 'Application')
     if (-not $ffMpegInPath -and $env:GITHUB_WORKFLOW) {
+        "::group::Installing FFMpeg" | Out-Host
         sudo apt update | Out-Host
         sudo apt install ffmpeg | Out-Host
+        "::endgroup::" | Out-Host
     }
 }
 
@@ -14,6 +16,15 @@ describe New-Media {
             Select-Object -ExpandProperty Duration |
             Should -Be "00:00:05"
 
+        Remove-Item $tmpOutPath
+    }
+
+    it 'Can create audio' {
+        $tmpOutPath = Join-Path ([io.pATH]::GetTempPath()) "sine$(Get-Random).mp3"
+        New-Media -Sine -OutputPath $tmpOutPath -Duration "00:00:05" |
+            Get-Media |
+            Select-Object -ExpandProperty Duration |
+            Should -BeLike "00:00:05*"
         Remove-Item $tmpOutPath
     }
 }
@@ -33,6 +44,48 @@ describe Convert-Media {
 
         Remove-Item $tmpOutPath
         Remove-Item $converted.Fullname
+    }
+
+    it 'Can use an extension to -Resize while converting' {
+        $tmpOutPath  = Join-Path ([io.pATH]::GetTempPath()) "testsrc$(Get-Random).mp4"
+        $tmpOutPath2 = Join-Path ([io.pATH]::GetTempPath()) "testsrc$(Get-Random).mp4" 
+        $converted   = New-Media -TestSource testsrc -OutputPath $tmpOutPath -Duration "00:00:05" | 
+            Convert-Media -OutputPath $tmpOutPath2 -Resize '1024x720'
+        $converted | Get-Media | Select-Object -ExpandProperty Resolution | Should -Be '1024x720'
+    }
+}
+
+describe ConvertTo-Waveform {
+    it 'Can make waveforms' {
+        $tmpOutPath  = Join-Path ([io.pATH]::GetTempPath()) "sine$(Get-Random).mp3"
+        $tmpOutPath2 = Join-Path ([io.pATH]::GetTempPath()) "sine$(Get-Random).mp4"
+        $waveform    = New-Media -Sine -OutputPath $tmpOutPath -Duration "00:00:05" |
+            ConvertTo-WaveForm -Visualizer LineWaveform -OutputPath $tmpOutPath2
+        $waveform | 
+            Get-Media | 
+            Select-Object -ExpandProperty Duration | 
+            Should -BeLike "00:00:05*"
+        Remove-Item $tmpOutPath
+        Remove-Item $tmpOutPath2
+    }
+}
+
+
+describe ConvertTo-GIF {
+    it 'Can make gifs' {
+        $tmpOutPath  = Join-Path ([io.pATH]::GetTempPath()) "sine$(Get-Random).mp3"
+        $tmpOutPath2 = Join-Path ([io.pATH]::GetTempPath()) "sine$(Get-Random).mp4"
+        $tmpOutPath3 = Join-Path ([io.pATH]::GetTempPath()) "sine$(Get-Random).gif"
+        $waveform    = New-Media -Sine -OutputPath $tmpOutPath -Duration "00:00:05" |
+            ConvertTo-WaveForm -Visualizer LineWaveform -OutputPath $tmpOutPath2
+        $waveform | 
+            ConvertTo-Gif -OutputPath $tmpOutPath3 |
+            Get-Media | 
+            Select-Object -ExpandProperty Duration | 
+            Should -BeLike "00:00:05*"
+        Remove-Item $tmpOutPath
+        Remove-Item $tmpOutPath2
+        Remove-Item $tmpOutPath3
     }
 }
 
