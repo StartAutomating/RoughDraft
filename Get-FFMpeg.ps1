ad5db64e1d44901518d34a0082d0f9588a526b09
James Brundage <james.brundage@start-automating.com>
2021-11-30 18:26:34 -0800

    Get-FFMpeg:  making function

--- Get-FFMpeg.ps1
+++ Get-FFMpeg.ps1
@@ -1,51 +1,53 @@
-﻿<#
-.Synopsis
+﻿function Get-FFMpeg
+{
+    <#
+    .Synopsis
+        Gets FFMpeg
+    .Description
+        Gets FFMpeg, if installed
+    .Example
+        Get-FFMpeg
+    .Link
+        Use-FFMpeg
+    #>
+    [Outputtype([string])]
+    param(
+    # The path to FFMpeg
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]
+    $FFMpegPath
+    )
 
-.Description
-
-.Example
-    Get-FFMpeg
-.Link
-    Use-FFMpeg
-#>
-param(
-# The path to FFMpeg
-[Parameter(ValueFromPipelineByPropertyName)]
-[string]
-$FFMpegPath
-)
-
-if ($script:KnownFFMpegPath) {
-    return $script:KnownFFMpegPath
-}
-
-
-if ($ffMpegPath) {
-    $ffMpegAtPath = $ExecutionContext.SessionState.InvokeCommand.GetCommand($ffMpegPath, 'Application')
-    if ($ffMpegAtPath) {
-        $script:KnownFFMpegPath = $ffMpegAtPath.Source
+    if ($script:KnownFFMpegPath) {
         return $script:KnownFFMpegPath
     }
-}
 
-$ffMpegInPath = $ExecutionContext.SessionState.InvokeCommand.GetCommand('ffmpeg', 'Application')
-if ($ffMpegInPath) {
-    $script:KnownFFMpegPath = $ffMpegInPath.Source
-    return $script:KnownFFMpegPath
-}
 
-if ($env:ProgramFiles) {
-    $ffMpegInProgramFiles = 
-        $ExecutionContext.SessionState.InvokeCommand.GetCommand((
-            Join-Path (Join-Path (Join-Path $env:ProgramFiles ffmpeg) 'bin') 'ffmpeg.exe'
-        ), 'Application')
+    if ($ffMpegPath) {
+        $ffMpegAtPath = $ExecutionContext.SessionState.InvokeCommand.GetCommand($ffMpegPath, 'Application')
+        if ($ffMpegAtPath) {
+            $script:KnownFFMpegPath = $ffMpegAtPath.Source
+            return $script:KnownFFMpegPath
+        }
+    }
+
+    $ffMpegInPath = $ExecutionContext.SessionState.InvokeCommand.GetCommand('ffmpeg', 'Application')
+    if ($ffMpegInPath) {
+        $script:KnownFFMpegPath = $ffMpegInPath.Source
+        return $script:KnownFFMpegPath
+    }
+
+    if ($env:ProgramFiles) {
+        $ffMpegInProgramFiles = 
+            $ExecutionContext.SessionState.InvokeCommand.GetCommand((
+                Join-Path (Join-Path (Join-Path $env:ProgramFiles ffmpeg) 'bin') 'ffmpeg.exe'
+            ), 'Application')
     
-    if ($ffMpegInProgramFiles) {
-        $script:KnownFFMpegPath = $ffMpegInProgramFiles.Source
-        return $script:KnownFFMpegPath
+        if ($ffMpegInProgramFiles) {
+            $script:KnownFFMpegPath = $ffMpegInProgramFiles.Source
+            return $script:KnownFFMpegPath
+        }
     }
+
+    throw "Cannot find ffmpeg"
 }
-
-throw "Cannot find ffmpeg"
-
-
