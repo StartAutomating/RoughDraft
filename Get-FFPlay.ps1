d071ae83e3f2fdeb9f1a045f8f2f9502396f1084
James Brundage <james.brundage@start-automating.com>
2021-11-30 18:26:54 -0800

    Get-FFPlay: making function

--- Get-FFPlay.ps1
+++ Get-FFPlay.ps1
@@ -1,38 +1,53 @@
-﻿param(
-$ffplayPath
-)
+﻿function Get-FFPlay {
+    <#
+    .Synopsis
+        Gets FFMpeg
+    .Description
+        Gets FFMpeg, if installed
+    .Example
+        Get-FFMpeg
+    .Link
+        Use-FFMpeg
+    #>
+    [OutputType([string])]
+    param(
+    # The path to FFMpeg
+    [Parameter(ValueFromPipelineByPropertyName)]
+    $ffplayPath
+    )
 
-if ($script:KnownffplayPath) {
-    return $script:KnownffplayPath
-}
+    process {
+        if ($script:KnownffplayPath) {
+            return $script:KnownffplayPath
+        }
 
 
-if ($ffplayPath) {
-    $ffplayAtPath = $ExecutionContext.SessionState.InvokeCommand.GetCommand($ffplayPath, 'Application')
-    if ($ffplayAtPath) {
-        $script:KnownffplayPath = $ffplayAtPath.Source
-        return $script:KnownffplayPath
-    }
-}
+        if ($ffplayPath) {
+            $ffplayAtPath = $ExecutionContext.SessionState.InvokeCommand.GetCommand($ffplayPath, 'Application')
+            if ($ffplayAtPath) {
+                $script:KnownffplayPath = $ffplayAtPath.Source
+                return $script:KnownffplayPath
+            }
+        }
 
-$ffplayInPath = $ExecutionContext.SessionState.InvokeCommand.GetCommand('ffplay', 'Application')
-if ($ffplayInPath) {
-    $script:KnownffplayPath = $ffplayInPath.Source
-    return $script:KnownffplayPath
-}
+        $ffplayInPath = $ExecutionContext.SessionState.InvokeCommand.GetCommand('ffplay', 'Application')
+        if ($ffplayInPath) {
+            $script:KnownffplayPath = $ffplayInPath.Source
+            return $script:KnownffplayPath
+        }
 
-if ($env:ProgramFiles) {
-    $ffplayInProgramFiles = 
-        $ExecutionContext.SessionState.InvokeCommand.GetCommand((
-            Join-Path (Join-Path (Join-Path $env:ProgramFiles ffmpeg) 'bin') 'ffplay.exe'
-        ), 'Application')
+        if ($env:ProgramFiles) {
+            $ffplayInProgramFiles = 
+                $ExecutionContext.SessionState.InvokeCommand.GetCommand((
+                    Join-Path (Join-Path (Join-Path $env:ProgramFiles ffmpeg) 'bin') 'ffplay.exe'
+                ), 'Application')
     
-    if ($ffplayInProgramFiles) {
-        $script:KnownffplayPath = $ffplayInProgramFiles.Source
-        return $script:KnownffplayPath
+            if ($ffplayInProgramFiles) {
+                $script:KnownffplayPath = $ffplayInProgramFiles.Source
+                return $script:KnownffplayPath
+            }
+        }
+
+        throw "Cannot find ffplay"
     }
 }
-
-throw "Cannot find ffplay"
-
-
