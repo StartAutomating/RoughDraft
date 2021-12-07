96981b6e47bcf6427964f533508757778defa75f
James Brundage <james.brundage@start-automating.com>
2021-12-06 18:51:33 -0800

    Adding Test workflow for RoughDraft action and WriteExtensions.md.RoughDraft.ps1

---
+++ WriteExtensions.md.RoughDraft.ps1
@@ -0,0 +1,17 @@
+﻿$extensionList = Get-RoughDraftExtension 
+
+$extensionsMarkdown = @(
+    '|' + ("DisplayName","Synopsis","Extends" -join '|') + '|'
+    '|' + (':-','-','-:' -join '|') +'|'
+    foreach ($ext in $extensionList) {
+        $extendsList = $ext.Extends -join ','
+        '|' + (
+            $ext.DisplayName, ($ext.Synopsis -replace '[\r\n]'), $extendsList -join '|'
+        ) + '|'        
+    }
+) -join [Environment]::NewLine
+
+
+$extensionsMarkdown | Set-Content .\RoughDraftExtensions.md -Encoding UTF8
+Get-Item .\RoughDraftExtensions.md |
+    Add-Member NoteProperty CommitMessage "Updating Extensions.md [skip ci]" -Force -PassThru
