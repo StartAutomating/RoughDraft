b0656de0c949fe5f98a757c06353e8a98b2af284
James Brundage <james.brundage@start-automating.com>
2021-12-08 02:28:21 -0800

    Updating Workflow Definition

--- RoughDraft.GitHubWorkflow.psdevops.ps1
+++ RoughDraft.GitHubWorkflow.psdevops.ps1
@@ -1,3 +1,8 @@
 ﻿#requires -Module PSDevOps
-New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish |
+Push-Location $PSScriptRoot
+Import-BuildStep -ModuleName RoughDraft
+
+New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, MakeRoughDraft |
     Set-Content .\.github\workflows\TestAndPublish.yml -Encoding UTF8 -PassThru
+
+Pop-Location
