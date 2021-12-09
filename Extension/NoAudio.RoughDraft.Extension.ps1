df6196301e5af3626c890f17aef3a52d0da5c808
James Brundage <@github.com>
2021-12-08 21:08:26 -0800

    Adding NoAudio extension (extension for Split-Media)

---
+++ Extension/NoAudio.RoughDraft.Extension.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.Synopsis
+    Removes Audio
+.Description
+    Removes audio streams
+#>
+# It's an extension
+[Runtime.CompilerServices.Extension()]
+# that extends Split-Media            
+[Management.Automation.Cmdlet("Split","Media")]
+# that is not inherited
+[ComponentModel.Inheritance("Inherited")]
+param(
+# If set, will remove audio from input media.
+[Alias('RemoveAudio')]
+[switch]
+$NoAudio
+)
+
+"-c"
+"copy"
+"-an"
+
