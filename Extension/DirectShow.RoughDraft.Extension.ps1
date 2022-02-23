10fabc6907fcb648baedd5fafae54c48778e96f3
James Brundage <james.brundage@start-automating.com>
2022-02-23 01:43:31 -0800

    Adding DirectShow Extension (#33)

---
+++ Extension/DirectShow.RoughDraft.Extension.ps1
@@ -0,0 +1,26 @@
+﻿<#
+.Synopsis
+    DirectShow Extension
+.Description
+      
+#>
+[Management.Automation.Cmdlet("Receive","Media")]
+[Management.Automation.Cmdlet("Show","Media")]
+param(
+[Parameter(Mandatory)]
+[switch]
+$DirectShow,
+
+[string]
+$VideoDevice,
+
+[string]
+$AudioDevice
+)
+
+
+'-f','dshow'
+'-i', "$(@(
+    if ($VideoDevice) {"video=$videoDevice"}
+    if ($AudioDevice) {"audio=$AudioDevice"}    
+) -join ':')"
