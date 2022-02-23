f42af1b94e7c3c86da7720b991037b8346af93cb
James Brundage <james.brundage@start-automating.com>
2022-02-23 01:43:48 -0800

    Adding VideoForLinux Extension (#34)

---
+++ Extension/VideoForLinux.RoughDraft.Extension.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.Synopsis
+    VideoForLinux Extension
+.Description
+    
+#>
+[Management.Automation.Cmdlet("Receive","Media")]
+[Management.Automation.Cmdlet("Show","Media")]
+param(
+[Parameter(Mandatory)]
+[Alias('Video4Linux','V4L','Video4linux2', 'V4L2')]
+[switch]
+$VideoForLinux,
+
+[string]
+$VideoDevice
+)
+
+
+'-f','video4linux'
+'-i', "$VideoDevice"
+
