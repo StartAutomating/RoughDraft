386a36227e293f164101248938657888bc42a3eb
James Brundage <@github.com>
2022-02-27 17:42:50 -0800

    Updating VideoForLinux extension:  Extending Send-Media

--- Extension/VideoForLinux.RoughDraft.Extension.ps1
+++ Extension/VideoForLinux.RoughDraft.Extension.ps1
@@ -2,20 +2,22 @@
 .Synopsis
     VideoForLinux Extension
 .Description
-    
+    The VideoForLinux Extension lets you use linux video devices (i.e. as /dev/video0)        
 #>
 [Management.Automation.Cmdlet("Receive","Media")]
+[Management.Automation.Cmdlet("Send","Media")]
 [Management.Automation.Cmdlet("Show","Media")]
 param(
+# If set, will use video4linux2 as the input type
 [Parameter(Mandatory)]
 [Alias('Video4Linux','V4L','Video4linux2', 'V4L2')]
 [switch]
 $VideoForLinux,
 
+# The name of the video device.
 [string]
 $VideoDevice
 )
-
 
 '-f','video4linux2'
 '-i', "$VideoDevice"
