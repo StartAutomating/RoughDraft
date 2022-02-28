9702d13170d24cbbb4013ae592f7250f992d9149
James Brundage <@github.com>
2022-02-27 17:43:16 -0800

    Updating DirectShow extension:  Extending Send-Media

--- Extension/DirectShow.RoughDraft.Extension.ps1
+++ Extension/DirectShow.RoughDraft.Extension.ps1
@@ -2,18 +2,24 @@
 .Synopsis
     DirectShow Extension
 .Description
-      
+    The DirectShow extension lets you used DirectShow input devices
+
 #>
 [Management.Automation.Cmdlet("Receive","Media")]
+[Management.Automation.Cmdlet("Send","Media")]
 [Management.Automation.Cmdlet("Show","Media")]
 param(
+# If set, will use DirectShow as the input type
 [Parameter(Mandatory)]
+[Alias('dshow')]
 [switch]
 $DirectShow,
 
+# The name of a DirectShow video device
 [string]
 $VideoDevice,
 
+# The name of a DirectShow audio device
 [string]
 $AudioDevice
 )
