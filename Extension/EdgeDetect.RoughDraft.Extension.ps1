185f337b9cc44bd9e0e57842e7a6a314fb36a4c2
James Brundage <@github.com>
2022-03-05 17:36:04 -0800

    Adding EdgeDetect Extension (#53)

---
+++ Extension/EdgeDetect.RoughDraft.Extension.ps1
@@ -0,0 +1,44 @@
+<#
+.SYNOPSIS
+    EdgeDetect Extension
+.DESCRIPTION
+    Uses the edgedetect filter to outline edges found in a video.
+.LINK
+    https://ffmpeg.org/ffmpeg-filters.html#edgedetect
+#>
+[Management.Automation.Cmdlet("Edit","Media")]
+param(
+[Parameter(Mandatory)]
+[Alias('DetectEdge', 'DetectEdges')]
+[switch]
+$EdgeDetect,
+
+# The low threshold.  By default, ~0.86
+[ValidateRange(0,1)]
+[float]
+$EdgeLowThreshold,
+
+# The high threshold.  By default ~.196
+[ValidateRange(0,1)]
+[float]
+$EdgeHighThreshold,
+
+# The edge detection mode.  By default, wires.
+[ValidateSet('wires','colormix','canny')]
+[string]
+$EdgeDetectMode
+)
+
+$filterArgs = @(
+    if ($EdgeLowThreshold) {
+        "low=$EdgeLowThreshold"
+    }
+    if ($EdgeHighThreshold) {
+        "high=$edgehighThreshold"
+    }
+    if ($EdgeDetectMode) {
+        "mode=$($EdgeDetectMode.ToLower())"
+    }
+) -join ':'
+"-vf"
+"edgedetect=$filterArgs" -replace '=$'
