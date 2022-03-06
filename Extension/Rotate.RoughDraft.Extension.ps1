7ac8be814e239fa97569b23802b4acce21596ce8
James Brundage <@github.com>
2022-03-05 17:30:49 -0800

    Rotate extension:  Allowing arbitrary rotation (#47)

--- Extension/Rotate.RoughDraft.Extension.ps1
+++ Extension/Rotate.RoughDraft.Extension.ps1
@@ -3,6 +3,10 @@
     Rotates a video
 .Description
     Applies the rotate filter to rotate a video.
+.LINK
+    https://ffmpeg.org/ffmpeg-filters.html#rotate
+.LINK
+    https://ffmpeg.org/ffmpeg-filters.html#transpose-1    
 #>
 # It's an extension
 [Runtime.CompilerServices.Extension()]
@@ -15,12 +19,29 @@
 param(
 # If provided, the video file will be rotated
 [Parameter(Mandatory)]
-[ValidateSet('90','180','270')]
-[Uint32]
-$Rotate
+[string]
+$Rotate,
+
+[string]
+$RotateFillColor
 )
 
-if ($Rotate) { # If we're going to rotate the video
-    "-vf" # Use the [transpose videofilter](https://ffmpeg.org/ffmpeg-filters.html#transpose-1)
-    "`"transpose=$(if ($rotate  -eq 90) { 1 } elseif ($rotate -eq 180) { 2 } elseif ($rotate -eq 270)  { 3})`""
+$intRotate = $Rotate -as [int]
+if ($Rotate) { # If we're going to rotate the video 
+    "-vf" 
+    if ($null -ne $intRotate -and 
+        -not ($intRotate % 90)) { # And the angle is an int and a factor of 90 degrees
+        # Use the [transpose videofilter](https://ffmpeg.org/ffmpeg-filters.html#transpose-1)
+        $intRotate = $intRotate % 360
+        "`"transpose=$(if ($intRotate  -eq 90) { 1 } elseif ($intRotate -eq 180) { 2 } elseif ($intRotate -eq 270)  { 3})`""
+    }
+    else {
+        $filterArgs = @(
+            "a=$rotate"
+            if ($RotateFillColor){
+                "fillcolor=$rotateFillColor"
+            }
+        ) -join ':'
+        "`"rotate=$filterArgs`""
+    }
 }
