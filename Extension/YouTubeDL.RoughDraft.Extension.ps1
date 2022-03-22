025360d23027b0f4f117ff1608fa5b3880b2e442
James Brundage <@github.com>
2022-03-21 21:39:10 -0700

    YouTubeDL extension:  Adding help and .link

--- Extension/YouTubeDL.RoughDraft.Extension.ps1
+++ Extension/YouTubeDL.RoughDraft.Extension.ps1
@@ -1,5 +1,13 @@
+<#
+.SYNOPSIS
+    Youtube Downloader
+.DESCRIPTION
+    Extends Get-Media to enable the downloading of videos from YouTube and other sources, using YouTubeDL
+.LINK
+    http://ytdl-org.github.io/youtube-dl/
+#>
 [Management.Automation.Cmdlet("Get","Media")]
-# that is inherited (this is the default).
+# that is not inherited (this is the default).
 [ComponentModel.Inheritance("NotInherited")]
 param(
 [Parameter(Mandatory)]
