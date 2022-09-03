4c0240d944613588565e57ec41f57556aa357171
James Brundage <@github.com>
2022-09-03 16:39:38 -0700

    Use-FFMpeg:  Improving Error Handling (Fixes #108)

--- Use-FFMpeg.ps1
+++ Use-FFMpeg.ps1
@@ -42,11 +42,42 @@
         #endregion Handle -AsJob
 
         #region Run FFMpeg
-        Write-Verbose "Invoke FFMpeg with $($ffmpegArgument -join ' ')"
+        Write-Verbose "Invoke FFMpeg with $($ffmpegArgument -join ' ')"        
+        $outputLines = [Collections.ArrayList]::new()
         & $ffmpeg @FFMpegArgument *>&1 |
             . {
-                process {
+                process {                    
                     $line = $_
+                    $null = $outputLines.Add($line)
+                    $lineIsError = $line -match '^\[[^\]]+\]\sError'
+                    $errorMessage = @()
+                    if ($lineIsError) { # If the line is an error
+                        $errorLines = @(
+                            $outputLines[$outputLines.Count - 2] # the previous line should have some context.
+                            $line
+                        )
+                        $errorMessage = @(foreach ($errorLine in $errorLines) {
+                            $errorLine -replace '^\[[^\]]+\]\s{0,}'
+                        }) -join [Environment]::NewLine                        
+                    } elseif ($line -match '^Error') {
+                        $errorMessage = @(for ($outLineNumber = $outputLines.Count - 2; $outLineNumber; $outLineNumber--) {
+                            if ($outputLines[$outLineNumber] -notmatch '^\[') { break }
+                            $outputLines[$outLineNumber] -replace '^\[[^\]]+\]\s{0,}'
+                        }) -join [Environment]::NewLine
+                    }
+                    elseif ($line -match '^\[[^\]]+\]\sUnable') {
+                        $errorMessage = $line -replace '^\[[^\]]+\]\s{0,}'
+                    }
+
+                    if ($errorMessage) {
+                        $errorRecord = [Management.Automation.ErrorRecord]::new(
+                            [Exception]::new($errorMessage),
+                            "FFMpeg.Error",
+                            "NotSpecified",
+                            $FFMpegArgument
+                        )
+                        $psCmdlet.WriteError($errorRecord)
+                    }
                     "$line"
                 }
             }
