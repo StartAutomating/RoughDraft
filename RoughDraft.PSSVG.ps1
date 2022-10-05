850d74b8496ab7ff7bdf872dcd7e37136ab7e8ac
James Brundage <@github.com>
2022-10-05 15:18:09 -0700

    Updating RoughDraft.PSSVG.ps1 (Fixes #127)

--- RoughDraft.PSSVG.ps1
+++ RoughDraft.PSSVG.ps1
@@ -22,7 +22,7 @@ $outputPath = Join-Path $psScriptRoot Assets
 $outputPath = Join-Path $outputPath RoughDraft.svg
 =<svg> @(
     $psChevron
-    =<svg.use> -href '#psChevron'  -X '10%' -Y '0%' -Height '100%' -Width '20%' -Stroke '#4488ff'
+    =<svg.use> -href '#psChevron'  -X '10%' -Y '0%' -Height '100%' -Width '20%' -Stroke '#4488ff' -Fill transparent
     =<svg.text> @(
         =<svg.tspan> "Rough" -FontFamily 'serif' -Rotate 1 
         =<svg.tspan> "Draft" -FontFamily 'serif' -Dx '-0.3em' -Rotate 1 -Dy '0.1em'
