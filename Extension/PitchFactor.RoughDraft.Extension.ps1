19a811fd1831f580e4355535f785eb36e55a250c
James Brundage <@github.com>
2022-11-01 20:35:59 -0700

    Adding PitchFactor Extension (Fixes #157)

---
+++ Extension/PitchFactor.RoughDraft.Extension.ps1
@@ -0,0 +1,53 @@
+
+<#
+.SYNOPSIS
+    Adjust Pitch by a factor
+.DESCRIPTION
+    Adjust audit pitch by a factor.
+
+    This is done by reampling the audio, resetting the sample rate, and adjusting the tempo 
+.LINK
+    https://ffmpeg.org/ffmpeg-filters.html#asetrate
+.LINK
+    https://ffmpeg.org/ffmpeg-filters.html#aresample
+.LINK
+    https://ffmpeg.org/ffmpeg-filters.html#atempo
+.EXAMPLE
+    Edit-Media -InputPath .\a.mp3 -PitchFactor 1.25 # Adjust pitch by 125% (making audio more high pitched)
+.EXAMPLE
+    Edit-Media -InputPath .\a.mp3 -PitchFactor 0.9 # Adjust pitch by 90% (making audio lower pitched)
+#>
+
+[Management.Automation.Cmdlet('(?>Edit|Show)', 'Media')]
+param(
+# The Pitch Factor.
+[Parameter(Mandatory)]
+[ValidateRange(0.5,2)]
+[double]
+$PitchFactor,
+
+# The pitch factor sample rate.
+# When using Edit-Media, this will be automatically be detected from the first audio stream.
+# When using Show-Media, this defaults to 44100
+[int]
+$PitchFactorSampleRate
+)
+
+$PitchFactorSampleRate = 
+    if ($PitchFactorSampleRate) {
+        $PitchFactorSampleRate
+    } elseif ($mediaInfo) {
+        $mediaInfo.streams |
+            Where-Object codec_type -eq 'audio' |
+            Where-Object sample_rate |
+            Select-Object -first 1 -expand sample_rate
+
+    } else {
+        44100
+    }
+
+
+'-af'
+"asetrate=$PitchFactorSampleRate*$PitchFactor,aresample=$PitchFactorSampleRate,atempo=$([Math]::round(1/$PitchFactor,4))"
+ 
+
