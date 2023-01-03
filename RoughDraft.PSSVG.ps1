a171c471cdd2e3fc1341fc5c122ad2bcb0a6f412
James Brundage <@github.com>
2023-01-03 14:20:12 -0800

    Improving Logo

--- RoughDraft.PSSVG.ps1
+++ RoughDraft.PSSVG.ps1
@@ -21,10 +21,15 @@ $psChevron =
 $outputPath = Join-Path $psScriptRoot Assets
 $outputPath = Join-Path $outputPath RoughDraft.svg
 =<svg> @(
+    =<svg.defs> @(
+        =<svg.style> -Type 'text/css' @'
+@import url('https://fonts.googleapis.com/css?family=Space%20Grotesk')
+'@
+    )
     $psChevron
-    =<svg.use> -href '#psChevron'  -X '10%' -Y '0%' -Height '100%' -Width '20%' -Stroke '#4488ff' -Fill transparent
+    =<svg.use> -href '#psChevron'  -X '5%' -Y '0%' -Height '100%' -Width '20%' -Fill '#4488ff' -Class foreground-fill
     =<svg.text> @(
-        =<svg.tspan> "Rough" -FontFamily 'serif' -Rotate 1 
-        =<svg.tspan> "Draft" -FontFamily 'serif' -Dx '-0.3em' -Rotate 1 -Dy '0.1em'
-    ) -FontSize 36 -Fill '#4488ff' -X 50% -DominantBaseline 'middle' -TextAnchor 'middle' -Y 50%
+        =<svg.tspan> "Rough" -FontFamily 'serif' -Rotate 1 -Style "font-family: 'Space Grotesk';" 
+        =<svg.tspan> "Draft" -FontFamily 'serif' -Dx '-0.3em' -Rotate 2 -Dy '0.2em' -Style "font-family: 'Space Grotesk';" 
+    ) -FontSize 36 -Fill '#4488ff' -X 50% -DominantBaseline 'middle' -TextAnchor 'middle' -Y 50% -Class foreground-fill
 ) -ViewBox 300, 100 -OutputPath $outputPath
