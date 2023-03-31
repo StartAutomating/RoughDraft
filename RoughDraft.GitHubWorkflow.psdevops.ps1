e229cfa541529d364d36d2dba1f37752686504df
James Brundage <@github.com>
2023-03-31 14:23:08 -0700

    Auto-building Get-RoughDraftExtension (Fixes #184)

--- RoughDraft.GitHubWorkflow.psdevops.ps1
+++ RoughDraft.GitHubWorkflow.psdevops.ps1
@@ -3,9 +3,9 @@ Push-Location $PSScriptRoot
 Import-Module .\RoughDraft.psd1 -Global
 Import-BuildStep -ModuleName RoughDraft
 
-New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, 
-    TestPowerShellOnLinux, 
-    TagReleaseAndPublish, 
+New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis,
+    TestPowerShellOnLinux,
+    TagReleaseAndPublish,
     BuildRoughDraft -OutputPath .\.github\workflows\TestAndPublish.yml 
 
 New-GitHubWorkflow -On Issue, Demand -Job RunGitPub -Name OnIssueChanged -OutputPath .\.github\workflows\OnIssue.yml
