45185913cba75035108a0c730e6695a89b03e66b
James Brundage <@github.com>
2024-02-09 15:22:32 -0800

    feat: Adding Repair-Media ( Fixes #208 )

---
+++ Commands/Media/Repair-Media.ps1
@@ -0,0 +1,48 @@
+function Repair-Media
+{
+    <#
+    .SYNOPSIS
+        Repairs Media
+    .DESCRIPTION
+        Repairs Media using any number of extensions.
+    .EXAMPLE
+        Get-ChildItem -Filter *.m3u | Repair-Media -FixPlayListPath
+    #>
+    param(
+    # One or more input paths
+    [Parameter(Mandatory,Position=0,ValueFromPipelineByPropertyName)]
+    [Alias('Fullname')]
+    [string[]]
+    $InputPath,
+    
+    # An optional output path
+    [Parameter(Position=1,ValueFromPipelineByPropertyName)]
+    [string]
+    $OutputPath
+    )
+
+    dynamicParam {
+        $myCmd = $MyInvocation.MyCommand
+        Get-RoughDraftExtension -CommandName $myCmd -DynamicParameter -NoMandatoryDynamicParameter -DynamicParameterSetName "__AllParameterSets" 
+    }
+
+    process {
+        $in = @{} + $PSBoundParameters
+        
+        #region Run Repair-Media Extensions
+        :repairingMedia do {
+            Get-RoughDraftExtension -CommandName $myCmd -CanRun -ExtensionParameter $in |
+                . Get-RoughDraftExtension -Run |
+                . { process {
+                    $inObj = $_
+                    if ($inObj.ExtensionOutput) {                        
+                        $inObj.extensionOutput
+                    }
+                    if ($inObj.Done) {
+                        break repairingMedia
+                    }
+                } }            
+        } while ($false)
+        #region Run Repair-Media Extensions
+    }        
+}
