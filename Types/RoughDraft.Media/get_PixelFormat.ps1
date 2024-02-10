1232d6e488a820245a26eb7f5028721fd1aaefb1
James Brundage <@github.com>
2024-02-10 14:21:16 -0800

    feat: RoughDraft.Media.PixelFormat ( Fixes #218 )

---
+++ Types/RoughDraft.Media/get_PixelFormat.ps1
@@ -0,0 +1,14 @@
+<#
+.SYNOPSIS
+    Gets a Video or Image PixelFormat
+.DESCRIPTION
+    Gets the pixel format the first video or image stream.
+#>
+foreach ($streamInfo in $this.Streams) {
+    if ($streamInfo.codec_type -ne 'video') { continue }
+    if ($streamInfo.pix_fmt) {
+        return $streamInfo.pix_fmt
+    }
+}
+
+
