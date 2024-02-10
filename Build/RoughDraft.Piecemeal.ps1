44560680f8d8378c02e4b7205bed6bfc2dfc0f91
James Brundage <@github.com>
2024-02-10 12:05:09 -0800

    feat: Moving RoughDraft.Piecemeal.ps1 into /Build ( Fixes #204 )

--- Build/RoughDraft.Piecemeal.ps1
+++ Build/RoughDraft.Piecemeal.ps1
@@ -1,3 +1,3 @@
-Push-Location $PSScriptRoot
+Push-Location ($PSScriptRoot | Split-Path)
 Install-Piecemeal -ExtensionModule 'RoughDraft' -ExtensionModuleAlias 'rd' -ExtensionTypeName 'RoughDraft.Extension' -OutputPath '.\Get-RoughDraftExtension.ps1'
 Pop-Location
