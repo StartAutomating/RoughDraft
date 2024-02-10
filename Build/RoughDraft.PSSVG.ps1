acad182c483953c739e673a1e304e8f2e19a25ee
James Brundage <@github.com>
2024-02-10 12:13:03 -0800

    feat: Moving RoughDraft.PSSVG.ps1 into /Build ( Fixes #204 )
    Also, using tagless format for PSSVG and simplifying design implementation.

---
+++ Build/RoughDraft.PSSVG.ps1
@@ -0,0 +1,18 @@
+#requires -Module PSSVG
+$psChevron = Invoke-restMethod https://pssvg.start-automating.com/Examples/PowerShellChevron.svg
+
+$outputPath = Join-Path ($psScriptRoot | Split-Path) Assets
+$outputPath = Join-Path $outputPath RoughDraft.svg
+svg @(
+    svg.defs @(
+        svg.style -Type 'text/css' @'
+@import url('https://fonts.googleapis.com/css?family=Space Grotesk')
+'@
+    )
+    $psChevron.svg.symbol.OuterXml
+    svg.use -href '#psChevron'  -X '5%' -Y '0%' -Height '100%' -Width '20%' -Fill '#4488ff' -Class foreground-fill
+    svg.text @(
+        svg.tspan "Rough" -FontFamily 'serif' -Rotate 1 -Style "font-family: 'Space Grotesk';" 
+        svg.tspan "Draft" -FontFamily 'serif' -Dx '-0.3em' -Rotate 2 -Dy '0.2em' -Style "font-family: 'Space Grotesk';" 
+    ) -FontSize 36 -Fill '#4488ff' -X 50% -DominantBaseline 'middle' -TextAnchor 'middle' -Y 50% -Class foreground-fill
+) -ViewBox 300, 100 -OutputPath $outputPath
