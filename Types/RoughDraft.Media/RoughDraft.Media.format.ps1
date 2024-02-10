5f798ad4f78ef89bfee0df114da6b32281fceb83
James Brundage <@github.com>
2024-02-10 15:28:09 -0800

    fix: Basic Media Formatting ( Fixes #30 )
    Reordering and fixing properties

--- Types/RoughDraft.Media/RoughDraft.Media.format.ps1
+++ Types/RoughDraft.Media/RoughDraft.Media.format.ps1
@@ -1,16 +1,16 @@
-Write-FormatView -TypeName RoughDraft.Media -Property FileName, MediaType, Length -VirtualProperty @{
-    Length = {
-        if ($_.Length) {
-            if ($_.Length / 1gb -eq 1) {
-                '' + $([Math]::Round($_.Length / 1gb, 2)) + 'gb'
+Write-FormatView -TypeName RoughDraft.Media -Property FileName, FileSize, MediaType -VirtualProperty @{
+    FileSize = {
+        if ($_.FileSize) {
+            if ($_.FileSize / 1gb -eq 1) {
+                '' + $([Math]::Round($_.FileSize / 1gb, 2)) + 'gb'
             }
-            elseif ($_.Length / 1mb -ge 1) {
-                '' + $([Math]::Round($_.Length / 1mb, 2)) + 'mb'
+            elseif ($_.FileSize / 1mb -ge 1) {
+                '' + $([Math]::Round($_.FileSize / 1mb, 2)) + 'mb'
             }
-            elseif ($_.Length -gt 1kb) {
-                '' + $([Math]::Round($_.Length / 1kb, 2)) + 'kb'
-            } elseif ($_ -isnot [IO.DirectoryInfo]) {
-                $_.Length
+            elseif ($_.FileSize -gt 1kb) {
+                '' + $([Math]::Round($_.FileSize / 1kb, 2)) + 'kb'
+            } else {
+                $_.FileSize
             }
         }
     }
