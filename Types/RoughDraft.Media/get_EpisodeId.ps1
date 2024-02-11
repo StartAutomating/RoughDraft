68807c1939e9a7f3566fd840ac24b6867d1472ba
James Brundage <@github.com>
2024-02-11 10:41:45 -0800

    feat: RoughDraft.Media.get/set_EpisodeId ( Fixes #235 )

---
+++ Types/RoughDraft.Media/get_EpisodeId.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+    Gets EpisodeID Metadata
+.DESCRIPTION
+    Gets the EpisodeID Metadata, if present.
+#>
+param()
+if ($this.'.Metadata'.EpisodeID) {
+    return $this.'.Metadata'.EpisodeID
+} elseif ($this.'.Metadata'.TVEN) {
+    return $this.'.Metadata'.TVEN
+}
---
+++ Types/RoughDraft.Media/set_EpisodeId.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+    Sets the EpisodeID Metadata
+.DESCRIPTION
+    Changes the EpisodeID metadata for a media file (persistently, if possible)
+.NOTES
+    EpisodeID is stored within the ID3 tag `TVEN`.
+#>
+param()
+$propertyToSet = @{TVEN="$args"}
+if (-not $this.'.Metadata') {
+    $this.'.Metadata' = [Ordered]@{}
+}
+$this.'.Metadata'.EpisodeID = $propertyToSet.TVEN
+Set-Media -Property $propertyToSet
