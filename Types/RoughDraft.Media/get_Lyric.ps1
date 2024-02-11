0a743427db26f998307add8dc029e89dbe5f85d1
James Brundage <@github.com>
2024-02-11 12:25:07 -0800

    fix: Fixing Lyric Culture Lookup ( Fixes #239 )
    Also defaulting to English

--- Types/RoughDraft.Media/get_Lyric.ps1
+++ Types/RoughDraft.Media/get_Lyric.ps1
@@ -5,8 +5,21 @@
     Gets the InitialKey Metadata, if present.
 #>
 param()
+
+# If we already have lyrics
 if ($this.'.Metadata'.Lyrics) {
+    # return them
     return ($this.'.Metadata'.Lyrics)
-} elseif ($this.'.Metadata'."lyrics-$([cultureinfo]::ThreeLetterISOLanguageName)") {
-    return ($this.'.Metadata'."lyrics-$([cultureinfo]::ThreeLetterISOLanguageName)")
+}
+# Otherwise, if lyrics exist for the current UI culture
+elseif ($this.'.Metadata'."lyrics-$([cultureinfo]::CurrentUICulture.ThreeLetterISOLanguageName)")
+{
+    # return them
+    return ($this.'.Metadata'."lyrics-$([cultureinfo]::CurrentUICulture.ThreeLetterISOLanguageName)")
+} 
+# Otherwise, if English lyrics exist
+elseif ($this.'.Metadata'.'lyrics-eng') 
+{
+    # return them.
+    return $this.'.Metadata'.'lyrics-eng'
 }
