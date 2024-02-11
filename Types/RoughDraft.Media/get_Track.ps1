c5a6932e70694cd9fd9cea28a9478d2143f4782b
James Brundage <@github.com>
2024-02-11 11:19:27 -0800

    feat: RoughDraft.Media.get/set_Track ( Fixes #247 )

---
+++ Types/RoughDraft.Media/get_Track.ps1
@@ -0,0 +1,8 @@
+<#
+.SYNOPSIS
+    Gets Track Metadata
+.DESCRIPTION
+    Gets the Track Metadata, if present.
+#>
+param()
+return $this.'.Metadata'.Track
---
+++ Types/RoughDraft.Media/set_Track.ps1
@@ -0,0 +1,13 @@
+<#
+.SYNOPSIS
+    Sets the Track Metadata
+.DESCRIPTION
+    Changes the Track metadata for a media file (persistently, if possible)
+#>
+param()
+$propertyToSet = @{track="$args"}
+if (-not $this.'.Metadata') {
+    $this.'.Metadata' = [Ordered]@{}
+}
+$this.'.Metadata'.Track = $propertyToSet.track
+Set-Media -Property $propertyToSet
