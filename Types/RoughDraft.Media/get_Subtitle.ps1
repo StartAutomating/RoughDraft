b95c59fb4166123006a1255b6c403066f105b84a
James Brundage <@github.com>
2024-02-11 11:14:24 -0800

    feat: RoughDraft.Media.get/set_Subtitle ( Fixes #245 )

---
+++ Types/RoughDraft.Media/get_Subtitle.ps1
@@ -0,0 +1,14 @@
+<#
+.SYNOPSIS
+    Gets Subtitle Metadata
+.DESCRIPTION
+    Gets the Subtitle Metadata, if present.
+.NOTES
+    This refers to the Subtitle of a file, not the Subtitles for a file.
+#>
+param()
+if ($this.'.Metadata'.Subtitle) {
+    return ($this.'.Metadata'.Subtitle)
+} elseif ($this.'.Metadata'.TIT3) {
+    return ($this.'.Metadata'.TIT3)
+}
---
+++ Types/RoughDraft.Media/set_Subtitle.ps1
@@ -0,0 +1,17 @@
+<#
+.SYNOPSIS
+    Sets the Subtitle Metadata
+.DESCRIPTION
+    Changes the Subtitle metadata for a media file (persistently, if possible)
+.NOTES
+    Subtitle is stored within the ID3 tag `TIT3`.
+
+    This refers to the subtitle of a file, not the subtitles _for_ a file.
+#>
+param()
+$propertyToSet = @{TIT3="$args"}
+if (-not $this.'.Metadata') {
+    $this.'.Metadata' = [Ordered]@{}
+}
+$this.'.Metadata'.Show = $propertyToSet.TIT3
+Set-Media -Property $propertyToSet
