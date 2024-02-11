3c64eaf1619bae8bffa65899dcf23435e230825c
James Brundage <@github.com>
2024-02-11 10:52:35 -0800

    feat: RoughDraft.Media.get/set_InitialKey ( Fixes #238 )

---
+++ Types/RoughDraft.Media/get_InitialKey.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+    Gets InitialKey Metadata
+.DESCRIPTION
+    Gets the InitialKey Metadata, if present.
+#>
+param()
+if ($this.'.Metadata'.InitialKey) {
+    return ($this.'.Metadata'.InitialKey)
+} elseif ($this.'.Metadata'.TKEY) {
+    return ($this.'.Metadata'.TKEY)
+}
---
+++ Types/RoughDraft.Media/set_InitialKey.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+    Sets the InitialKey Metadata
+.DESCRIPTION
+    Changes the InitialKey metadata for a media file (persistently, if possible)
+.NOTES
+    InitialKey is stored within the ID3 tag `TKEY`.
+#>
+param()
+$propertyToSet = @{TKEY="$args"}
+if (-not $this.'.Metadata') {
+    $this.'.Metadata' = [Ordered]@{}
+}
+$this.'.Metadata'.InitialKey = $propertyToSet.TKEY
+Set-Media -Property $propertyToSet
