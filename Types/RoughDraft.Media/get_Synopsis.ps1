0659659ab2a6e0823536c6d3c64522fa958932ea
James Brundage <@github.com>
2024-02-11 11:15:52 -0800

    feat: RoughDraft.Media.get/set_Synopsis ( Fixes #246 )

---
+++ Types/RoughDraft.Media/get_Synopsis.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+    Gets Synopsis Metadata
+.DESCRIPTION
+    Gets the Synopsis Metadata, if present.
+#>
+param()
+if ($this.'.Metadata'.Synopsis) {
+    return ($this.'.Metadata'.Synopsis)
+} elseif ($this.'.Metadata'.ldes) {
+    return ($this.'.Metadata'.ldes)
+}
---
+++ Types/RoughDraft.Media/set_Synopsis.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+    Sets the Synopsis Metadata
+.DESCRIPTION
+    Changes the Synopsis metadata for a media file (persistently, if possible)
+.NOTES
+    Synopsis is stored within the ID3 tag `ldes`.
+#>
+param()
+$propertyToSet = @{ldes="$args"}
+if (-not $this.'.Metadata') {
+    $this.'.Metadata' = [Ordered]@{}
+}
+$this.'.Metadata'.Synopsis = $propertyToSet.ldes
+Set-Media -Property $propertyToSet
