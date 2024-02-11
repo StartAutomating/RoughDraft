9353caeefb8711cb8046dd3494277e9cc43d2266
James Brundage <@github.com>
2024-02-11 10:10:23 -0800

    feat: RoughDraft.Media.get/set_Title ( Fixes #225 )

---
+++ Types/RoughDraft.Media/get_Title.ps1
@@ -0,0 +1,8 @@
+<#
+.SYNOPSIS
+    Gets Title Metadata
+.DESCRIPTION
+    Gets the Title Metadata, if present.
+#>
+param()
+return $this.'.Metadata'.Title
---
+++ Types/RoughDraft.Media/set_Title.ps1
@@ -0,0 +1,13 @@
+<#
+.SYNOPSIS
+    Sets the Title Metadata
+.DESCRIPTION
+    Changes the Title metadata for a media file (persistently, if possible)
+#>
+param()
+$propertyToSet = @{title="$args"}
+if (-not $this.'.Metadata') {
+    $this.'.Metadata' = [Ordered]@{}
+}
+$this.'.Metadata'.title = $propertyToSet.title
+Set-Media -Property $propertyToSet
