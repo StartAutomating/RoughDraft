460e6415e8d9815985b975a4d7d1e48a2ec11ce6
James Brundage <@github.com>
2024-02-11 11:35:51 -0800

    feat: RoughDraft.Media.get/set_Artist ( Fixes #249 )

---
+++ Types/RoughDraft.Media/get_Artist.ps1
@@ -0,0 +1,13 @@
+<#
+.SYNOPSIS
+    Gets Artist Metadata
+.DESCRIPTION
+    Gets the Artist Metadata, if present.
+#>
+param()
+return $(
+    if ($this.'.Metadata'.Artist) {
+        $this.'.Metadata'.Artist
+    } else { $null }
+)
+
---
+++ Types/RoughDraft.Media/set_Artist.ps1
@@ -0,0 +1,13 @@
+<#
+.SYNOPSIS
+    Sets the Artist Metadata
+.DESCRIPTION
+    Changes the Artist metadata for a media file (persistently, if possible)
+#>
+param()
+$propertyToSet = @{artist="$args"}
+if (-not $this.'.Metadata') {
+    $this.'.Metadata' = [Ordered]@{}
+}
+$this.'.Metadata'.AlbumArtist = $propertyToSet.artist
+Set-Media -Property $propertyToSet
