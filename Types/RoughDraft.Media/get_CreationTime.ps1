4a4134ba32c26e6a19576dcbd36035fbda2270f2
James Brundage <@github.com>
2024-02-11 10:30:25 -0800

    feat: RoughDraft.Media.get/set_CreationTime ( Fixes #232 )

---
+++ Types/RoughDraft.Media/get_CreationTime.ps1
@@ -0,0 +1,13 @@
+<#
+.SYNOPSIS
+    Gets Creation Time Metadata
+.DESCRIPTION
+    Gets the Creation Time Metadata, if present.
+#>
+param()
+if ($this.'.Metadata'.CreationTime) {
+    return ($this.'.Metadata'.CreationTime -as [DateTime])
+} elseif ($this.'.Metadata'.creation_time) {
+    return ($this.'.Metadata'.creation_time -as [DateTime])
+}
+
---
+++ Types/RoughDraft.Media/set_CreationTime.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+    Sets the Creation Time Metadata
+.DESCRIPTION
+    Changes the Creation Time metadata for a media file (persistently, if possible)
+#>
+param()
+
+$dateTimeArgs = [DateTime]$($args[0])
+$datePortion = @($dateTimeArgs.ToString('s') -split '-', 2)[0]
+$propertyToSet = @{creation_time="$datePortion"}
+if (-not $this.'.Metadata') {
+    $this.'.Metadata' = [Ordered]@{}
+}
+$this.'.Metadata'.CreationTime = $propertyToSet.creation_time
+Set-Media -Property $propertyToSet
